fc8a69ac914a37a8ce72f4d1a854208a353bcced
miladev-ent <milwad.dev@gmail.com>
2022-11-21 20:53:48 +0330

    sesson 69

--- Modules/Mlk/Home/Http/Controllers/HomeController.php
+++ Modules/Mlk/Home/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace Mlk\Home\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 use Mlk\Advertising\Models\Advertising;
 use Mlk\Advertising\Repositories\AdvertisingRepo;
@@ -15,7 +16,10 @@ class HomeController extends Controller
 //        dd(Storage::disk('local')->get('private/milwad.txt'));
 //        dd(Storage::disk('local')->get('private/laravel-frame.png'));
 
-        $adv_top = $advertisingRepo->getAdvByLocation(Advertising::LOCATION_TOP_MAIN_PAGE)->latest()->first();
+        $adv_top = Cache::remember('top_advs', 2, function () use ($advertisingRepo) {
+            return $advertisingRepo->getAdvByLocation(Advertising::LOCATION_TOP_MAIN_PAGE)->latest()->first();
+        });
+//        $adv_top = $advertisingRepo->getAdvByLocation(Advertising::LOCATION_TOP_MAIN_PAGE)->latest()->first();
         $adv_bottom = $advertisingRepo->getAdvByLocation(Advertising::LOCATION_BOTTOM_MAIN_PAGE)->latest()->first();
 
         return view('Home::index', compact(['homeRepo', 'adv_top', 'adv_bottom']));
--- Modules/Mlk/Home/Repositories/HomeRepo.php
+++ Modules/Mlk/Home/Repositories/HomeRepo.php
@@ -2,6 +2,7 @@
 
 namespace Mlk\Home\Repositories;
 
+use Illuminate\Support\Facades\Cache;
 use Mlk\Article\Models\Article;
 use Mlk\Category\Models\Category;
 use Mlk\Role\Models\Permission;
@@ -12,6 +13,13 @@ class HomeRepo
     public function vip_posts()
     {
         return Article::query()->where('status', Article::STATUS_ACTIVE)->whereType(Article::TYPE_VIP)->latest();
+
+//        return Cache::remember('vip_posts', 2, function () {
+//            return Article::query()
+//                ->where('status', Article::STATUS_ACTIVE)
+//                ->whereType(Article::TYPE_VIP)
+//                ->latest();
+//        });
     }
 
     public function getActiveCategories()
